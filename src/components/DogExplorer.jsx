0b82527e953475c7468f3d6998ad5afcee0fe30e
Anne Martinez <martinezannemerlyn@gmail.com>
2025-02-03 08:38:25 +0100

    added fetching logic

--- src/components/DogExplorer.jsx
+++ src/components/DogExplorer.jsx
@@ -1,10 +1,29 @@
 import DogCard from "./DogCard";
+import { fetchDogs } from "../utils/network";
+import { useEffect, useState } from "react";
 
 const DogExplorer = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const getDogs = async () => {
+            const dogs = await fetchDogs();
+            if (dogs) setData(dogs);
+        };
+
+        getDogs();
+    }, []);
+
     return (
         <>
-            <h1>Dog Explorer Page</h1>
-            <DogCard />
+            {/* <h1>Dog Explorer Page</h1> */}
+            <div className="gap-2 grid grid-cols-1 md:grid-cols-3">
+                {data.length > 0 ? (
+                    data.map((item) => <DogCard key={item.id} item={item} />)
+                ) : (
+                    <p>Loading dogs...</p>
+                )}
+            </div>
         </>
     );
 };
--- src/components/DogStatus.jsx
+++ src/components/DogStatus.jsx
@@ -1,5 +1,26 @@
+import { fetchDogs, fetchDogStatus } from "../utils/network";
+
 const DogStatus = () => {
-  return <h1>Dog Status Page</h1>;
+    (async () => {
+        try {
+            const dogs = await fetchDogs();
+            const status = await fetchDogStatus();
+
+            if (!dogs || !status) {
+                throw new Error("Something went wrong");
+            }
+            console.log(dogs);
+            console.log(status);
+        } catch (error) {
+            console.error("Error fetching dogs:", error);
+        }
+    })();
+
+    return (
+        <>
+            <h1>Dog Status Page</h1>
+        </>
+    );
 };
 
 export default DogStatus;
